926443a02444a0694cc7d056582099a6fb6c24f6
Omar Ramirez <omarbenjaminrc@gmail.com>
2024-03-05 12:14:15 -0300

    Agregadas vistas y URLs para el departamento de tránsito. Agregados archivos de plantilla base_transito.html y base.html.

--- plataforma/views.py
+++ plataforma/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'plataforma/index.html')
+
+def departamento_transito(request):
+    return render(request, 'departamento_de_transito/departamento_transito.html')
